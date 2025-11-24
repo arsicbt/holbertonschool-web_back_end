ffe97b743de0631cfb46dada8aa1bc4d9ad301f1
arsicbt <arsinoechobert@gmail.com>
2025-11-24 16:20:36 +0100

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Concatenate two strings with type annotations.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings."""
+    return str1 + str2
