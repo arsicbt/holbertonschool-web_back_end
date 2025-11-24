55a11b5482b7c96ad9b87c69f118f4b7a7ec0d02
arsicbt <arsinoechobert@gmail.com>
2025-11-24 16:23:09 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Return the floor of a float as an integer with type annotations.
+"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
+    return int(n // 1)
