804f9b56981a545e8f63fb00214761a0e2a6dc38
arsicbt <arsinoechobert@gmail.com>
2025-11-26 14:10:31 +0100

    Fix: Add documentation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module that contains the function task_wait_random
+"""
 
 
 import asyncio
@@ -7,4 +10,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates and returns an asyncio Task that runs wait_random"""
     return asyncio.create_task(wait_random(max_delay))
