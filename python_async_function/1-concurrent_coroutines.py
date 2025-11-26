cbae92195dc4d3dff5b12e1bf3e5831388d54140
arsicbt <arsinoechobert@gmail.com>
2025-11-26 12:06:18 +0100

    Fix: Syntaxe erreur

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ from typing import List  # annoter le type
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Execute wait_random n times with max_delay
 
