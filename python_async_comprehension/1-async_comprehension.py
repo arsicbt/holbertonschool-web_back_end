026660be0cf2124a9c786c9199aa63645958abfa
arsicbt <arsinoechobert@gmail.com>
2025-11-27 23:01:52 +0100

    Fix: Syntaxe error

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehensio() -> List[float]:
+async def async_comprehension() -> List[float]:
     """ Collect 10 random numbers using an async comprehensing"""
     return [num async for num in async_generator()]
