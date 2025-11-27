21aa0d56608d9219ed173e9b9d1175c79541be2a
arsicbt <arsinoechobert@gmail.com>
2025-11-27 22:59:20 +0100

    Fix: Bad import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@
 from typing import List
 
 
-async_generator = __import__('0-async_comprehension').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehensio() -> List[float]:
