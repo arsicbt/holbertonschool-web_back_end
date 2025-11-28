e92718dee71e512dda8b71ef2b3431f38c5d0707
arsicbt <arsinoechobert@gmail.com>
2025-11-28 14:41:45 +0100

    Change comment syntaxe

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Measure runtime of parallel async comprehensions"""
+"""
+Measure runtime of parallel async comprehensions
+"""
 
 
 import asyncio
 import time
-from importlib import import_module as using
 
-async_comprehension = using('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
