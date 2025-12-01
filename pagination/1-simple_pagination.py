4bb687697cb766ede8de57a57cc5ff21ccf762fc
arsicbt <arsinoechobert@gmail.com>
2025-12-01 14:20:42 +0100

    fix: Add documentations

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" 
+Simple pagination
+"""
 
 import csv
 import math
@@ -32,6 +35,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Returns the appropriate page of the dataset """
 
         assert isinstance(page, int) and page > 0, "doit être positif"
         assert isinstance(page_size, int) and page_size > 0, "doit être +"
