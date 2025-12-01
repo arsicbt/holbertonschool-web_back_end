a3d34c57b0ff62c152f5a6ea61b1c6afb91afa40
arsicbt <arsinoechobert@gmail.com>
2025-12-01 15:31:29 +0100

    Deletion-resilient hypermedia pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -40,4 +40,30 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """ Returns a dictionary containing the following key-value pairs """
+        # liste complete des data à paginer
+        dataset = self.dataset()
+        total_lines = len(dataset)
+
+        # index: entier ? compris entre 0 et total_lines -1 ?
+        assert isinstance(index, int) and 0 <= index < total_lines
+        assert isinstance(page_size, int) and page_size > 0
+
+        # page demandée :
+        # prends les lignes du dataset de l’indice...
+        # ...index jusqu’à index + page_size - 1
+        data = dataset[index:index + page_size]
+
+        # index + taille réelle...
+        # ...(dernière page peut être plus courte que page_size)
+        next_index = index + len(data)
+        if next_index >= total_lines:
+            # none si on atteint la fin
+            next_index = None
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(data),
+            'data': data,
+        }
