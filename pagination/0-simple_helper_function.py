81ac06fc13c602fdd4cbffeb926c43f0f060a428
arsicbt <arsinoechobert@gmail.com>
2025-12-01 13:53:08 +0100

    fix: Add documentations

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+"""
+Module that contains a helper function for pagination
+"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
+    """ Returns a tuple of size two containing a start index """
 
     start = (page - 1) * page_size
     end = page_size * page
