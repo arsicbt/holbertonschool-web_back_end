edec742eabdea98e29e18d5b6e321fd4fbabd4f7
arsicbt <arsinoechobert@gmail.com>
2025-12-01 13:50:53 +0100

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+def index_range(page: int, page_size: int) -> tuple:
+    start = 0
+    end = 0
+
+    for i in range(0, page_size - 1):
+        start = (page - 1) * page_size
+        end = page_size * page
+
+    return (start, end)
