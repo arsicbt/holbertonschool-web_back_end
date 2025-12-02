8a83337ee05583dac65c804c657693361ece4e74
arsicbt <arsinoechobert@gmail.com>
2025-12-02 14:16:16 +0100

    fix: Add documentation

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""
+Module that inserts a new document in a MongoDB collection.
+"""
 
 
 from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a MongoDB collection."""
     li = []
     for to_insert in mongo_collection.find():
         li.insert(kwarg)
