f8c80209cea1a2a0c470a647b20e6766c7db49bf
arsicbt <arsinoechobert@gmail.com>
2025-12-02 14:15:19 +0100

    Insert a document in Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,4 +5,7 @@ from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
-    
+    li = []
+    for to_insert in mongo_collection.find():
+        li.insert(kwarg)
+    return li
