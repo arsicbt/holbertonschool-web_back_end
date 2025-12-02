7951b3c156122dce529b8884a7d5ee860271c36b
arsicbt <arsinoechobert@gmail.com>
2025-12-02 14:31:44 +0100

    fix: Syntaxe error

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -10,4 +10,4 @@ from pymongo import MongoClient
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a MongoDB collection."""
     inserted = mongo_collection.insert_one(kwargs)
-    return inserted.inserted_ids
+    return inserted.inserted_id
