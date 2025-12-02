4f33ead87cbc824342d51544342df53f8c96cc3a
arsicbt <arsinoechobert@gmail.com>
2025-12-02 15:32:45 +0100

    Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/venv python3
+"""
+Module that updates topics in a MongoDB collection.
+"""
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics for a school in a MongoDB collection."""
+    updated = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return updated
