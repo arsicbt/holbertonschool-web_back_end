29dbedaf9127ad04df22644b63443df7612571da
arsicbt <chobertarsinoe@gmail.com>
2025-12-09 16:16:39 +0100

    12. Let's create a report object

--- ES6_basic/12-main.js
+++ ES6_basic/12-main.js
@@ -5,3 +5,7 @@ const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
     ...createEmployeesObject('marketing', ['Sylvie'])
 };
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
