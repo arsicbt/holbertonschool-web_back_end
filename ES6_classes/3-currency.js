b30a9c9c9aca53e0cb81f8569f30929d33559017
arsicbt <chobertarsinoe@gmail.com>
2025-12-10 15:48:10 +0100

    feat: implement getter et setter, and display function

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -3,4 +3,24 @@ export default class Currency{
     this._code = code;
     this._name = name;
   }
+
+  get code() {
+    return this._code;
+  };
+
+  get name() {
+    return this._name;
+  };
+
+  set code(_code) {
+    return this._code;
+  };
+
+  set name(_name) {
+    return this._name;
+  };
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
