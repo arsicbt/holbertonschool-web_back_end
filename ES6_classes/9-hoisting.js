2725bc44186fea94765a9a0b6382955ea888f69c
arsicbt <chobertarsinoe@gmail.com>
2025-12-10 17:22:57 +0100

    Missing parameter: StudentHolberton constructor was missing the holbertonClass parameter - added it as the third parameter.

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -17,7 +17,7 @@ export class HolbertonClass {
 }
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
