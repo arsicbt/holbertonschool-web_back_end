9b4973b409870afabf1260cf206890d812bbd22c
arsicbt <chobertarsinoe@gmail.com>
2025-12-10 17:26:08 +0100

    Order of declarations: Moved the instantiation of class2019 and class2020 to after the class declarations, and student instantiations after that.

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,6 +1,3 @@
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -35,6 +32,9 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
