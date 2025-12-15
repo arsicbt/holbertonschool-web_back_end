8ec08ec167fd4a9adcf56d7d2521ee464b74be6f
arsicbt <chobertarsinoe@gmail.com>
2025-12-15 11:14:16 +0100

    fix: Handles type mismatches by converting student IDs to numbers for correct matching.

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,12 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
-  if(!Array.isArray(studentsList) || !Array.isArray(newGrades))
-    return []
+  if (!Array.isArray(studentsList) || !Array.isArray(newGrades)) return [];
 
-  // Récupérer les étudiants de la ville
-  const cityOfStudents = studentsList.filter(i => i.location === city);
-  // find cherche la note donnée
-  const studentsGraded = cityOfStudents.map(j => {
-    const gradeObject = newGrades.find(j => j.studentId === j.id);
+  const cityOfStudents = studentsList.filter(student => student.location === city);
+
+  const studentsGraded = cityOfStudents.map(student => {
+    const gradeObject = newGrades.find(g => Number(g.studentId) === Number(student.id));
     return {
-      ...j,
+      ...student,
       grade: gradeObject ? gradeObject.grade : 'N/A',
     };
   });
