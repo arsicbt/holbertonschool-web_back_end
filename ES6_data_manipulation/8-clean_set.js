7568e1cb02e730b9b904c0958505bc30a0283b54
arsicbt <chobertarsinoe@gmail.com>
2025-12-15 12:58:56 +0100

    fix: Change conditions order

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 export default function cleanSet(set, startString) {
-  if(!(set instanceof Set) || typeof startString !== 'string')
-    throw new Error('Bad argument: Bad argument(s) type(s)');
 
   if(startString === '')
     return '';
+
+  if(!(set instanceof Set) || typeof startString !== 'string')
+    throw new Error('Bad argument: Bad argument(s) type(s)');
 
   const filteredOutput = [...set]
     .filter(value => typeof value === 'string' && value.startsWith((startString)))
