36474042d1847f27670e2146f7e08142848e2d49
arsicbt <chobertarsinoe@gmail.com>
2025-12-15 13:06:58 +0100

    fix: Correctly handle empty string case

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
 export default function cleanSet(set, startString) {
 
-  if(startString === '')
-    return '';
-
-  if(!(set instanceof Set) || typeof startString !== 'string')
+  if (!(set instanceof Set)) {
     throw new Error('Bad argument: Bad argument(s) type(s)');
+  }
+
+  if (!startString) return '';
+
+  if (typeof startString !== 'string') {
+    throw new Error('Bad argument: Bad argument(s) type(s)');
+  }
 
   const filteredOutput = [...set]
     .filter(value => typeof value === 'string' && value.startsWith((startString)))
