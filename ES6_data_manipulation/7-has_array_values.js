db3fc05d63d15638d53b92576e73466851d163c1
arsicbt <chobertarsinoe@gmail.com>
2025-12-15 12:08:37 +0100

    fix: Syntax error in the condition

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 export default function hasValuesFromArray(set, array) {
-  if(!Array.isArray(array) || !Set.isSet(set))
+  if(!Array.isArray(array) || !(set instanceof Set))
     throw new Error('Bad argument: incorrect argument type');
 
   return array.every(value => set.has(value));
