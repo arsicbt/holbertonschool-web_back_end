d3db11ce1b61ec34a44cd20062a2e85611129c6e
arsicbt <chobertarsinoe@gmail.com>
2025-12-15 11:42:57 +0100

    fix: Handle if  the value is not possible

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   if(!Number(length) || !Number(position) || !Number(value))
-    console.log("Bad Argument: not a number")
+    throw new Error('"Bad Argument: not a number');
+
+  if(position >= length || position < 0)
+    throw new Error('Position outside range');
 
   const buffer = new ArrayBuffer(length);
   const int8data = new Int8Array(buffer);
