9bdf821fea80007546867ee81cbdb0f8da32aa0c
arsicbt <chobertarsinoe@gmail.com>
2025-12-17 12:25:39 +0100

    6. Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // all: attend une réussite, allSettled: attend un retour (réussi ou non)
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]);
+}
