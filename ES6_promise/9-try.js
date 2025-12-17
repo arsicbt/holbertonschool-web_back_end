a00500888f37ce23feca06dab3910ace7bee78a5
arsicbt <chobertarsinoe@gmail.com>
2025-12-17 14:20:20 +0100

    9. Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const res = mathFunction()
+    queue.push(res)
+  } catch (err) {
+    // transformer error en version lisible
+    queue.push(err.toString())
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
