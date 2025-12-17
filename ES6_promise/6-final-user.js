0f5701b5f08696b63c02e1ea913826bc03235443
arsicbt <chobertarsinoe@gmail.com>
2025-12-17 13:31:17 +0100

    fix: Use async function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  // all: attend une réussite, allSettled: attend un retour (réussi ou non)
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  }));
+  const photo = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: `${error.name}: ${error.message}`,
+  }));
+  return [user, photo];
 }
