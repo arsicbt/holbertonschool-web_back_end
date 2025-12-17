ebec40e051c6121690b5f3f3c4903b73798d146d
arsicbt <chobertarsinoe@gmail.com>
2025-12-17 13:27:50 +0100

    fix: Remove condition in 5-photo-rejected

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(fileName) {
-  if (typeof fileName != 'string')
-    return `${fileName} is not a string`
-
   return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
