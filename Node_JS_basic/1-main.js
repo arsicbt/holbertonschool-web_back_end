22e14ab6dd79f87bcf3fc71394bc623bf564fd0e
arsicbt <arsinoechobert@gmail.com>
2026-01-05 13:39:26 +0100

    fix: Write my code in the good file

---
+++ Node_JS_basic/1-main.js
@@ -0,0 +1,17 @@
+// affiche le message
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// encoding pour lecture du text
+process.stdin.setEncoding('utf8');
+
+// ecoute l'entrée utilisateur
+process.stdin.on('data', (data) => {
+  // enleve la saut de ligne + affiche le nom
+  const name = data.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+// ecoute la fin de stdin (Ctrl+D)
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
