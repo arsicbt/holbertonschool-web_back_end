ef8dd97843892e0b91c81b0945084f2b32fcd622
arsicbt <arsinoechobert@gmail.com>
2026-01-06 13:08:13 +0100

    fix: add status code

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,14 @@
+// 4-http.js
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
-  console.log('Serveur démarré sur http://localhost:1245');
+  console.log('Server listening on port 1245');
 });
+
+module.exports = app;
