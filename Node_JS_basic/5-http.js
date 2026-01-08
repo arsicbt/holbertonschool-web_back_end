f14781d52564cf48178b9994e4918afd0fa0e88f
arsicbt <arsinoechobert@gmail.com>
2026-01-08 16:33:25 +0100

    fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,4 +1,3 @@
-// 5-http.js
 const http = require('http');
 const path = require('path');
 const countStudents = require('./3-read_file_async');
@@ -8,35 +7,35 @@ const database = process.argv[2] ? path.resolve(process.argv[2]) : null;
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
-  countStudents(database)
-    .then((output) => {
-      res.statusCode = 200;
-      res.end(`This is the list of our students\n${output}`);
-    })
-    .catch(() => {
-      res.statusCode = 500;
-      res.end('Cannot load the database');
-    });
-
   if (req.url === '/') {
     res.statusCode = 200;
     res.end('Hello Holberton School!');
-    return null;
+    return;
   }
 
   if (req.url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
+
     if (!database) {
       res.statusCode = 500;
       res.end('Cannot load the database');
-      return null;
+      return;
     }
 
-    return null;
+    countStudents(database)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
+      });
+    return;
   }
 
   res.statusCode = 404;
   res.end('Not found');
-  return null;
 });
 
 app.listen(1245, () => {
