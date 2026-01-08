680526b0f6d7bdd4a4248567360c8fe1e7be7c2d
arsicbt <arsinoechobert@gmail.com>
2026-01-08 16:39:58 +0100

    add the principal file

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,1 +1,13 @@
-app = express
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = 1245;
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
