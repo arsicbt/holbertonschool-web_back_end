d15886428c5f3c4af07bfbcfd075cf8901137966
arsicbt <arsinoechobert@gmail.com>
2026-01-09 11:08:09 +0100

    fix: use a Router

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,11 @@
-import express from 'express';
-import AppController from '../controllers/AppController.js';
-import StudentsController from '../controllers/StudentsController.js';
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/', AppController.getHomepage);
-
 router.get('/students', StudentsController.getAllStudents);
-
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 export default router;
--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-const PORT = 1245;
 
 app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(1245);
 
-module.exports = app;
+export default app;
